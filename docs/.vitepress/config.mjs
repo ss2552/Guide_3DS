ac8c430ec84a30ff3d2456088bee22984d32d5e5
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-16 15:04:45 -0700

    config: add sidebar/navbar and initial translation support

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -6,9 +6,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vitepress'
 
+import * as i18n from './i18n'
+
 export default defineConfig({
   title: "3DS Hacks Guide",
   description: "A complete guide to 3DS custom firmware",
+  locales: {
+    root: i18n.en_US
+  },
   vite: {
     resolve: {
       alias: [
