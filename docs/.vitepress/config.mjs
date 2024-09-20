eb56fd730d5d254b5ed83dc6810a6380bab59e32
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-20 01:58:49 -0700

    config: add GitHub link to navbar

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,6 +19,11 @@ export default defineConfig({
   locales: {
     root: i18n.en_US
   },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/hacks-guide/Guide_3DS' }
+    ]
+  },
   vite: {
     resolve: {
       alias: [
