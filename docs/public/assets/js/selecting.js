c0410889ff1a06d9715ebef970cc45c758ec551c
ManiacOfGitHub <06crhenry@gmail.com>
2025-01-31 14:43:21 -0500

    consoleVersionSelect: add version prefix If they don't select "Ver.", they have Luma3DS and have skipped the CFW check.

--- docs/public/assets/js/selecting.js
+++ docs/public/assets/js/selecting.js
@@ -152,6 +152,7 @@ function can_mset9(major, minor, native, region, model) {
         - All models
 */
 function redirect() {
+    const prefix = document.getElementById("prefix").value;
     const major = document.getElementById("major").value;
     const minor = document.getElementById("minor").value;
     const nver = document.getElementById("nver").value;
@@ -170,13 +171,18 @@ function redirect() {
     if(isO3DS) model = DEVICE_O3DS
     else if(isN3DS) model = DEVICE_N3DS;
 
-    if (!validate_version(major, minor, nver, region, model)) {
+    if (prefix == '' || !validate_version(major, minor, nver, region, model)) {
         document.getElementById("result_invalidVersion").style.display = "block";
         return;
     }
 
     // Store selected version for some later pages
     sessionStorage.setItem("selected_version", JSON.stringify({major, minor, nver, region, model}));
+
+    if(prefix != "Ver.") {
+        window.location.href = "checking-for-cfw";
+        return true;
+    }
 
     const redirected = [
       can_soundhax,
