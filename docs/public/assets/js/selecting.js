5f4a020e98b9b10c8582d2c465c535d5c0d1023d
lily <lily@uwuu.ca>
2025-03-23 21:27:49 -0700

    skater: unstub

--- docs/public/assets/js/selecting.js
+++ docs/public/assets/js/selecting.js
@@ -87,12 +87,24 @@ function can_safecerthax(major, minor, native, region, model) {
     return false;
 }
 
-// super-skaterhax (stubbed due to cert changes)
+// super-skaterhax
 // N3DS only
 // EUR/JPN/USA: 11.15-11.17
 // KOR: 11.15-11.16 only, KOR does not have 11.17
 // CHN/TWN has no N3DS
 function can_superskaterhax(major, minor, native, region, model) {
+    let do_redirect = false;
+    // N3DS only
+    if(model == DEVICE_N3DS) {
+        if (major == 11) {
+            if (minor >= 15) do_redirect = true;
+        }
+    }
+
+    if (do_redirect) {
+        window.location.href = "installing-boot9strap-(super-skaterhax)";
+        return true;
+    }
     return false;
 }
 
@@ -130,7 +142,7 @@ function can_mset9(major, minor, native, region, model) {
         - 11.4 - 11.14
         - All regions
         - O3DS only
-    - super-skaterhax (stubbed)
+    - super-skaterhax
         - 11.15 - 11.17
         - USA / EUR / JPN / KOR
         - N3DS only
